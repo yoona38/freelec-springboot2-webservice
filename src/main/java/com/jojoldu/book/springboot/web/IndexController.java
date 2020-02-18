626838b52cd1492def01d0bd340fb529410cc54b
YOONA <zlxl0308@gmail.com>
2020-02-18 23:42:27 +0900

    add

---
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,25 @@
+package com.jojoldu.book.springboot.web;
+
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("post", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("posts/save")
+    public String postSave() {
+        return "posts-save";
+    }
+}
